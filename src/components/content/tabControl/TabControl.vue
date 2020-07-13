0d84fd9f2b3d5b94d08513528d53edfc86e49f3d
unknown <15039938104@163.com>
2020-07-13 19:00:59 +0800

    TabControl组件的封装

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" :key="item.background" 
+          class="tab-control-item" :class="{active:index===currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"TabControl",
+  data(){
+    return {
+      currentIndex:0,
+    }
+  },
+  methods:{
+    itemClick(index){
+      this.currentIndex = index;
+    }
+  },
+  props:{
+    titles:{
+      type:Array,
+      default(){
+        return [];
+      }
+    }
+  },
+  components:{},
+  watch:{},
+  computed:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style scoped>
+  .tab-control{
+    display: flex;
+    font-size: 14px;
+    text-align: center;
+    background-color: rgb(222, 241, 228);
+  }
+
+  .tab-control-item{
+    flex: 1;
+    height: 40px;
+    line-height: 40px;
+  }
+  .tab-control-item span{
+    padding: 5px;
+  }
+
+  .active{
+    color: var(--color-high-text);
+  }
+  .active span{
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,6 +4,73 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view/>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+
+
+
+
+
+
+
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li> 
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li> 
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li> 
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li> 
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li> 
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li> 
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li> 
+    </ul>
   </div>
 </template>
 
@@ -19,11 +86,13 @@ import NavBar from 'components/common/navbar/NavBar';
 import HomeSwiper from './childComps/HomeSwiper'
 // 导入推荐展示组件 RecommendView
 import RecommendView from './childComps/RecommendView'
+// 导入FeatureView
+import FeatureView from './childComps/FeatureView'
 
-
+import TabControl from 'components/content/tabControl/TabControl'
 
 // 导入请求数据getHomeMultidata
-import {getHomeMultidata} from 'network/home';
+import {getHomeMultidata,getHomeGoods} from 'network/home';
 
 
 
@@ -35,6 +104,11 @@ export default {
       // result:null,
       banners:[],
       recommends:[],
+      goods:{
+        'pop':{page:0,list:[]},
+        'news':{page:0,list:[]},
+        'sellnp':{page:0,list:[]},
+      }
     }
   },
   methods:{},
@@ -47,6 +121,8 @@ export default {
     // 抽离之后注册一下HomeSwiper
     HomeSwiper,
     RecommendView,
+    FeatureView,
+    TabControl,
   },
   watch:{},
   computed:{},
@@ -60,16 +136,36 @@ export default {
       this.banners = res.data.banner.list;
       // 获取recommends数据
       this.recommends = res.data.recommend.list;
+
+      // 2.请求商品数据
+      getHomeGoods('pop',1).then(res=>{
+        console.log(res);
+      })
     })
   },
   mounted(){}
 }
 </script>
 <style  scoped>
+  #home{
+    padding-top:44px ;
+  }
   .home-nav{
     /* 背景颜色值 */
     background-color: var(--color-tint-one);
     /* 文字颜色值 */
     color:#fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top:0;
+    z-index: 9;
+  }
+
+  .tab-control{
+    /* 会根据top的值，自动将position的值改为fixed 移动端适用很好，但是ie这些就不兼容了 */
+    position: sticky;
+    top: 44px;
   }
 </style>
